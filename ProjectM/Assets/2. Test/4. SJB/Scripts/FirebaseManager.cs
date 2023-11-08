29fa9a8924cf0cf8e2225b743d044ca088de7ee3
JB <sjbh3mc@gmail.com>
2023-11-08 11:21:37 +0900

    심준보 : 프로젝트 Init

---
+++ ProjectM/Assets/2. Test/4. SJB/Scripts/FirebaseManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    // 주석 테스트
+    void Start()
+    {
+        
+    }
+}
